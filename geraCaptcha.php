cbe11d5f9152fcfb2b8ddbb6f789bc34561a24f2
GuiSelair <guilherme.lima1997@hotmail.com>
2019-07-16 20:47:58 -0300

    Update:  Captcha

--- geraCaptcha.php
+++ geraCaptcha.php
@@ -1,33 +1,32 @@
 <?php
-   session_start(); // inicial a sessao
+   session_start(); 
    header("Content-type: image/jpeg"); // define o tipo do arquivo
 
     function captcha($largura,$altura,$tamanho_fonte,$quantidade_letras){
-        $imagem = imagecreate($largura,$altura); // define a largura e a altura da imagem
-        $fonte = "arial.ttf"; //voce deve ter essa ou outra fonte de sua preferencia em sua pasta
-        $preto  = imagecolorallocate($imagem,0,0,0); // define a cor preta
-        $branco = imagecolorallocate($imagem,255,255,255); // define a cor branca
+        $imagem = imagecreate($largura,$altura); // LARGURA E ALTURA DA CAPTCHA GERADA
+        $fonte = "arial.ttf"; // FONTE UTILIZADA
+        $preto  = imagecolorallocate($imagem,0,0,0); // COR DO FUNDO
+        $branco = imagecolorallocate($imagem,255,255,255); // COR DA LETRA
 
-        // define a palavra conforme a quantidade de letras definidas no parametro
+        // DEFINE DE MODO ALEATÓRIO AS LETRAS UTILIZADAS COM A QUANTIDADE PASSADA
         $palavra = substr(str_shuffle("AaBbCcDdEeFfGgHhIiJjKkLlMmNnPpQq
         RrSsTtUuVvYyXxWwZz23456789"),0,($quantidade_letras));
 
-        $_SESSION["palavra"] = $palavra; // atribui para a sessao a palavra gerada
+        $_SESSION["palavra"] = $palavra; // COLOCA A PALAVRA EM UMA SESSAO
         
+        // ANEXA AS LETRAS A IMAGEM
         for($i = 1; $i <= $quantidade_letras; $i++){
             imagettftext($imagem,$tamanho_fonte,rand(-25,25),($tamanho_fonte*$i),
             ($tamanho_fonte + 10),$branco,$fonte,substr($palavra,($i-1),1));
-            // atribui as letras a imagem
         }
-        imagejpeg($imagem); // gera a imagem
-        imagedestroy($imagem); // limpa a imagem da memoria
+        imagejpeg($imagem); // GERA IMAGEM
+        imagedestroy($imagem); // APAGA A IMAGEM DA MEMÓRIA, TORNANDO A IMAGEM ÚNICA
     }
 
-    $largura = $_GET["l"]; // recebe a largura
-    $altura = $_GET["a"]; // recebe a altura
-    $tamanho_fonte = $_GET["tf"]; // recebe o tamanho da fonte
-    $quantidade_letras = $_GET["ql"]; // recebe a quantidade de letras que o captcha terá
+    $largura = $_GET["l"]; 
+    $altura = $_GET["a"]; 
+    $tamanho_fonte = $_GET["tf"]; 
+    $quantidade_letras = $_GET["ql"]; 
     
     captcha($largura,$altura,$tamanho_fonte,$quantidade_letras);
-    // executa a funcao captcha passando os parametros recebidos
 ?>
